2e7f51ee2b77fb9dfa505befaa31d9713b8f551c
Pedro Lustosa <pedroeternalss@gmail.com>
2020-08-25 23:43:01 -0300

    Challenge - v1.0 - APS .NET CORE2.2 Entity FrameWork + SQL Server

---
+++ CadCORE2.2/Migrations/20200826004910_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadCORE2._2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nascimento",
+                table: "Users",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salario",
+                table: "Users",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salario",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nascimento",
+                table: "Users",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "Users",
+                type: "varchar(250)",
+                nullable: true);
+        }
+    }
+}
---
+++ CadCORE2.2/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadCORE2._2.Models
+{
+	public class User
+	{
+		//ID - Preenchido automaticamente.
+		[Key]
+		public int UserID { get; set; }
+		
+
+		//Nome Completo
+		[Column(TypeName ="nvarchar(250)")]
+		[Required(ErrorMessage ="Por favor preencha o Campo com seu Nome Completo.")]
+		[DisplayName("Nome Completo")]
+		public string  NomeCompleto { get; set; }
+		
+
+		//Nascimento
+		[Column(TypeName = "varchar(250)")]
+		[Required(ErrorMessage = "Por favor preencha o Campo com a data do seu Nascimento.")]
+		[DisplayName("Nascimento")]
+		public string Nascimento { get; set; }
+		
+
+		//Salário
+		[Column(TypeName = "varchar(250)")]
+		[Required(ErrorMessage = "Por favor preencha o Campo com o seu Salário.")]
+		[DisplayName("Salário")]
+		public string Salario { get; set; }
+	}
+}
